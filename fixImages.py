0a1491b3960439f128b8ae512bc40f56dff33ab4
Jan <jan.urankar@gmail.com>
2024-10-08 13:25:51 +0200

    Bug fixes and added dataset split

--- fixImages.py
+++ fixImages.py
@@ -1,5 +1,6 @@
 import os
 import shutil
+import random
 
 from PIL import Image
 
@@ -93,8 +94,10 @@ def copy_labels_imgs_to_data(path_list, DATA_DIR):
 
 
     # Copy images from all dirs to data
-    for img_path in img_path_list:
+    for idx, img_path in enumerate(img_path_list):
         try:
+            if idx % 200 == 0:
+                print(f"Processing {idx}/{len(img_path_list)} %")
             img_list = os.listdir(img_path)
             for img in img_list:
                 shutil.copy(os.path.join(img_path, img), imgs_dir)
@@ -116,17 +119,71 @@ def copy_labels_imgs_to_data(path_list, DATA_DIR):
 This function accepts the path to a directory with datasets and than copies all the images and labels from all datasets to our working directory in DATA_DIR
 """
 def copy_directory_labels_imgs_to_data(src_dir_path, DATA_DIR):
-    datasets = os.listdir(src_dir_path)
-    datasets_list = []
-    for dataset in datasets:
-        datasets_list.append(os.path.join(src_dir_path,dataset))
+    datasets_list = [ os.path.join(src_dir_path, name) for name in os.listdir(src_dir_path) if os.path.isdir(os.path.join(src_dir_path, name)) ]
     copy_labels_imgs_to_data(datasets_list, DATA_DIR)
     print("FINITOOOO")
 
 
+def prepare_dir(path):
+    if os.path.exists(path):
+        shutil.rmtree(path)
+    os.makedirs(path)
+
+"""
+This function accepts the path to a directory with dataset which only containt train and than splits the dataset into train, test and valid datasets
+dataset_split is a list of 3 integers which represent the percentage of the dataset that will be used for train, test and valid datasets
+"""
+def split_dataset(DATA_DIR_SRC, DATA_DIR_TARGET, dataset_split=[70, 20, 10]):
+    # Set up source dirs
+    IMG_DIR = os.path.join(DATA_DIR_SRC, "images", "train")
+    LABELS_DIR = os.path.join(DATA_DIR_SRC, "labels", "train")
+
+    # Define target dirs for images and labels
+    target_dirs = {subdir: (os.path.join(DATA_DIR_TARGET, "images", subdir),
+                            os.path.join(DATA_DIR_TARGET, "labels", subdir))
+                   for subdir in ['train', 'test', 'valid']}
+
+    # Create/clean target directories
+    for img_dir, label_dir in target_dirs.values():
+        prepare_dir(img_dir)
+        prepare_dir(label_dir)
+
+    # Split dataset
+    dir_list = os.listdir(IMG_DIR)
+    random.shuffle(dir_list)
+
+    train_split = dataset_split[0] * len(dir_list) // 100
+    test_split = (dataset_split[0] + dataset_split[1]) * len(dir_list) // 100
+
+    for idx, img_file in enumerate(dir_list):
+        if idx % 500 == 0:
+            print(f"Processing {idx}/{len(dir_list)}")
+
+        img_path = os.path.join(IMG_DIR, img_file)
+        label_file_name = os.path.splitext(img_file)[0] + ".txt"
+        label_file_path = os.path.join(LABELS_DIR, label_file_name)
+
+        if os.path.exists(label_file_path):
+            if idx < train_split:
+                target = 'train'
+            elif idx < test_split:
+                target = 'test'
+            else:
+                target = 'valid'
+
+            shutil.copy(img_path, target_dirs[target][0])  # Copy image
+            shutil.copy(label_file_path, target_dirs[target][1])  # Copy label
+        else:
+            print(f"No label file for: {img_file}")
+
+    print("Finished splitting dataset")
+
+
 if __name__ == '__main__':
     DATA_DIR = os.path.join(os.getcwd(), 'data')
+    DATA_DIR_target = os.path.join(os.getcwd(), 'data_split')
     IMG_DIR = os.path.join(DATA_DIR, 'images', "train")
     LABEL_DIR = os.path.join(DATA_DIR, 'labels', "train")
-    # copy_directory_labels_imgs_to_data("D:\Sola\Magisterska\SlikeDataseti\DISEASE_DETECTION\CORN\CORN_DISEASE", DATA_DIR)
-    resize_images(DATA_DIR)
+    # copy_directory_labels_imgs_to_data("C:\\Users\\Jan\\Downloads\\fhb_wheat_datasets", DATA_DIR)
+    # resize_images(DATA_DIR)
+    split_dataset(DATA_DIR, DATA_DIR_target, dataset_split=[70, 20, 10])
